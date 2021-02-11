b25633066f10ed8193d2bdb91c085f8f38ab826c
adhemukhlis <adhemukhlis@gmail.com>
2021-02-12 02:40:18 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ua8ih4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ua8ih4)
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ApiWilayah from './CascadeApiWilayah';
+class App extends Component {
+  state = {
+    address: ''
+  };
+  onChange = (value, all) => {
+    this.setState({ address: all.map(s => s.label).join(', ') });
+  };
+  render() {
+    const { address } = this.state;
+    return (
+      <div style={style.container}>
+        <h2 style={style.bottomSpace}>Select Address Component</h2>
+        <ApiWilayah
+          style={style.bottomSpace}
+          size='large'
+          onChange={this.onChange}
+          placeholder='Set Address'
+        />
+        <i>{address}</i>
+      </div>
+    );
+  }
+}
+const style = {
+  container: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  bottomSpace: { marginBottom: '2em' }
+};
+export default App;
